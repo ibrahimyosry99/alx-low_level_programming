4bff34fce270ea312b961b271b1439a4c6c3d701
ibrahim_yousry <ebrahemysry@gmail.com>
2023-03-21 10:57:31 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,34 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-
-void print_alphabet(void)
-{
-	char alp = 'a';
-
-	while (alp <= 'z')
-	{
-		_putchar (alp);
-		alp++;
-
-	}
-	_putchar ('\n');
-
-#include "main.h"
-
-/**
  * print_alphabet_x10 - func
  *
  * Return - 0
  */
 void print_alphabet_x10(void)
 {
-
-  for (int i=0; i<11;i++)
+  for(int i=0 ;i,11;i++)
     {
       print_alphabet();
     }
