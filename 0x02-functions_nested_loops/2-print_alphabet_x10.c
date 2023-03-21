eae6a90d4ac365a30e18562b9d2acbeb40b81aeb
ibrahim_yousry <ebrahemysry@gmail.com>
2023-03-21 10:59:57 -0700

    l

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,13 @@ void print_alphabet_x10(void)
 {
   for(int i=0 ;i,11;i++)
     {
-      print_alphabet();
+      char alp='a';
+      	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+
     }
-  _putchar ('\n');
-}
